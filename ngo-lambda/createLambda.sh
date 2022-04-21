939de148d9e7c2f27d9c773418fa7234208e1c53
Doriordan2 <72385836+Doriordan2@users.noreply.github.com>
2022-04-21 19:29:55 +0100

    Versioning Updates

--- ngo-lambda/createLambda.sh
+++ ngo-lambda/createLambda.sh
@@ -26,10 +26,10 @@ sed -i "s|/home/ec2-user/managedblockchain-tls-chain.pem|./certs/managedblockcha
 sudo yum install gcc-c++ -y
 
 echo Install Node.js. We will use v10.x.
-curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.0/install.sh | bash
+curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.39.1/install.sh | bash
 . ~/.nvm/nvm.sh
-nvm install lts/dubnium
-nvm use lts/dubnium
+nvm install 14
+nvm use 14
 cd src
 npm install
 cd ..
